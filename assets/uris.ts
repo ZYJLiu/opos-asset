2ed72a7c0671401567d528dd77a2e89bf1e2caee
John <johnzhengyuliu@gmail.com>
2023-08-21 19:40:02 -0500

    fix url

--- assets/uris.ts
+++ assets/uris.ts
@@ -1,10 +1,10 @@
 export const uris = [
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Climate/Climate.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Closedcube/Closedcube.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Consensus/Consensus.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/DeveloperPortal/DeveloperPortal.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/DeveloperToolkit/DeveloperToolkit.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/OpenCube/OpenCube.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/SagaPhone/SagaPhone.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Security/Security.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/Climate/Climate.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/Closedcube/Closedcube.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/Consensus/Consensus.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/DeveloperPortal/DeveloperPortal.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/DeveloperToolkit/DeveloperToolkit.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/OpenCube/OpenCube.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/SagaPhone/SagaPhone.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/Security/Security.json",
 ];
