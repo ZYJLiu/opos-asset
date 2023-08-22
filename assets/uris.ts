b62ded23cca02509eb676c791471b52430dae9ee
John <johnzhengyuliu@gmail.com>
2023-08-21 19:34:17 -0500

    assets

---
+++ assets/uris.ts
@@ -0,0 +1,10 @@
+export const uris = [
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Climate/Climate.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Closedcube/Closedcube.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Consensus/Consensus.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/DeveloperPortal/DeveloperPortal.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/DeveloperToolkit/DeveloperToolkit.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/OpenCube/OpenCube.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/SagaPhone/SagaPhone.json",
+  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/assets/Security/Security.json",
+];
